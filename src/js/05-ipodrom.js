e8ce96cbce329beeee5ed3c73d155fd118d08b49
Sasha Fesenko <sashafesenkooo@gmail.com>
2023-01-23 14:54:37 +0200

    add file

--- src/js/05-ipodrom.js
+++ src/js/05-ipodrom.js
@@ -13,22 +13,23 @@ const refs = {
     tableBody: document.querySelector('.js-results-table > tbody'),
 }
 
-console.log(`Заезд начался, ставки не принимаются`);
-console.log('Победил ${1}, финишировав за ${1} времени');
-console.log('Заезд окончен принимаются ставки');
+const promise = horses.map(run);
+console.log(promise);
 
+console.log(`Заезд начался, ставки не принимаются`);
+
+run('Mango').then(x => console.log(x));
+// console.log('Победил ${1}, финишировав за ${1} времени');
+// console.log('Заезд окончен принимаются ставки');
 
 function run(horse) {
     return new Promise((resolve, reject) => {
         const time = getRandomTime(2000, 3500);
         setTimeout(() => {
-            resolve({ horse, time })
+            resolve({horse, time})
         }, time);
     })
 };
-
-run('Mango').then(x => console.log(x)).catch(e => console.log(e))
-
 
 function getRandomTime(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
